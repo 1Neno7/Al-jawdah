d1ae107573d9c494167c7e60f00788fd14585855
Mustafa Al-jawdah <102661029+1Neno7@users.noreply.github.com>
2022-08-05 15:25:45 +0300

    Update nft.js

--- nft.js
+++ nft.js
@@ -22,12 +22,12 @@ for (let i = 0; i < buttonsLength; i++) {
 
 /* ========== video size start ========== */
 var video = document.getElementById("nftbvideo");
-if (innerHeight / 1.282 + innerHeight >= innerWidth) {
+if (window.innerHeight / 1.282 + window.innerHeight >= window.innerWidth) {
   video.style.width = "100vmax";
   video.style.height = "auto";
 }
 window.addEventListener("resize", () => {
-  if (innerHeight / 1.282 + innerHeight >= innerWidth) {
+  if (window.innerHeight / 1.282 + window.innerHeight >= window.innerWidth) {
     video.style.width = "100vmax";
     video.style.height = "auto";
   } else {
